ff85fae9a6dd9260b7c967ed588f5774bfba7327
Aleksei Anisimov <anisimov.aleksei.v@gmail.com>
2024-07-09 19:47:32 +0300

    Commit 2

--- app/src/main/java/ru/yandex/practicum/sprint8koh19/MainActivity.kt
+++ app/src/main/java/ru/yandex/practicum/sprint8koh19/MainActivity.kt
@@ -1,6 +1,7 @@
 package ru.yandex.practicum.sprint8koh19
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -17,5 +18,6 @@ class MainActivity : AppCompatActivity() {
             insets
         }
 
+        Log.d("SPRINT_8", "Hello, world!")
     }
 }
